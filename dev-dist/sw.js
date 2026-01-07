f25c138512504045952d3928a6b5385eacb1ef39
Sankalp Sachan <sankalpsachan2007@gmail.com>
2026-01-08 01:34:12 +0530

    Adding Backend

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.ktm0lcen50g"
+    "revision": "0.bhujsjhf6t"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
