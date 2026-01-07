90c47c4c5813bd12f20a1544d3d356382889304c
Sankalp Sachan <sankalpsachan2007@gmail.com>
2026-01-07 18:26:59 +0530

    hu

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.8bevm59u7po"
+    "revision": "0.ktm0lcen50g"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
