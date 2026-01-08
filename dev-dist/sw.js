2853fc987e7fd556a98585445772577b6afea634
Sankalp Sachan <sankalpsachan2007@gmail.com>
2026-01-08 10:56:07 +0530

    new addition

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.nm76l3drcko"
+    "revision": "0.dl0j0t0vut8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
