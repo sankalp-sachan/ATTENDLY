8ea4b3052bb602f994c5f5aa97d4bc305783d361
Sankalp Sachan <sankalpsachan2007@gmail.com>
2026-01-09 17:56:32 +0530

    Changing Developer Contact Details

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.fa5efstchs8"
+    "revision": "0.nup5dvc7ci"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
