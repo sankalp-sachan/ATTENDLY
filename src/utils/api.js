63978236dc37dc412039fdf5e5e58d834eae037d
Sankalp Sachan <sankalpsachan2007@gmail.com>
2026-01-26 10:28:15 +0530

    adding server via link

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://127.0.0.1:5000/api',
+    baseURL: 'https://attendly-backend-pe5k.onrender.com/api',
 });
 
 // Add a request interceptor to attach the token
